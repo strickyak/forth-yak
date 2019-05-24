829b4d0792307446d24daba44621b01e2038e224
strick <strick@yak.net>
2019-05-24 00:44:04 -0700

    indent -kr

--- forth-yak.cxx
+++ forth-yak.cxx
@@ -2,64 +2,65 @@
 
 namespace forth_yak {
 
-char* Mem;
-int MemLen;
-C HerePtr;  // points to Here variable
-C LatestPtr;  // points to Latest variable
-C Ds;  // data stack ptr
-C Rs;  // return stack ptr
-C Ip;  // instruction ptr
-C W;  // W register
-const char* Argv0;
-OpFn* Ops[LINELEN];
-int Debug;
+    char *Mem;
+    int MemLen;
+    C HerePtr;			// points to Here variable
+    C LatestPtr;		// points to Latest variable
+    C Ds;			// data stack ptr
+    C Rs;			// return stack ptr
+    C Ip;			// instruction ptr
+    C W;			// W register
+    const char *Argv0;
+    OpFn *Ops[LINELEN];
+    int Debug;
 
-void Fatal(const char* msg, int x) {
+    void Fatal(const char *msg, int x) {
 	fprintf(stderr, "***\n* Fatal: %s: %s [%d]\n", Argv0, msg, x);
 	exit(13);
-}
-
-void DumpMem() {
-  D(stderr, "Dump: Rs=%d  Ds=%d  Ip=%d {\n", Rs, Ds, Ip);
-  for (int i = 0; i < MemLen; i++) {
-  	char m = Mem[i];
-  	if (m != 0) {
+    } void DumpMem() {
+	D(stderr, "Dump: Rs=%d  Ds=%d  Ip=%d {\n", Rs, Ds, Ip);
+	for (int i = 0; i < MemLen; i++) {
+	    char m = Mem[i];
+	    if (m != 0) {
 		char c = ' ';
-		if ( 32 <= m && m <= 126 ) {
-			c = m;
+		if (32 <= m && m <= 126) {
+		    c = m;
 		}
 		C x;
 		if (i + S < MemLen) {
-			x = Get(i);
+		    x = Get(i);
 		}
 
 		if (i >= Rs) {
-			D(stderr, "  [%8d+Rs] %3d %c %12lld\n", i-Rs, m, c, x);
+		    D(stderr, "  [%8d+Rs] %3d %c %12lld\n", i - Rs, m, c,
+		      x);
 		} else if (i >= Ds) {
-			D(stderr, "  [%8d+Ds] %3d %c %12lld\n", i-Ds, m, c, x);
+		    D(stderr, "  [%8d+Ds] %3d %c %12lld\n", i - Ds, m, c,
+		      x);
 		} else {
-			D(stderr, "  [%8d] %3d %c %12lld\n", i, m, c, x);
+		    D(stderr, "  [%8d] %3d %c %12lld\n", i, m, c, x);
 		}
+	    }
 	}
-  }
-  D(stderr, "}\n");
-}
+	D(stderr, "}\n");
+    }
 
-void OpPLUS() {
+    void OpPLUS() {
 	C x = Get(Ds);
-	C y = Get(Ds+S);
+	C y = Get(Ds + S);
 	Put(Ds, 0);
-	Put(Ds+S, x+y);
+	Put(Ds + S, x + y);
 	Ds += S;
-}
-void OpDOT() {
+    }
+
+    void OpDOT() {
 	C x = Get(Ds);
 	Ds += S;
-	printf("%lld ", (long long)x);
+	printf("%lld ", (long long) x);
 	fflush(stdout);
-}
+    }
 
-void MakeOp(Opcode code, OpFn fn, const char* name) {
+    void MakeOp(Opcode code, OpFn fn, const char *name) {
 	Ops[code] = fn;
 
 	C latest = Get(LatestPtr);
@@ -68,73 +69,73 @@ void MakeOp(Opcode code, OpFn fn, const char* name) {
 
 	Put(here, latest);
 	here += S;
-	strcpy(Mem+here, name);
+	strcpy(Mem + here, name);
 	here += strlen(name) + 1;
 	Put(here, code);
 	here += S;
 	Put(HerePtr, here);
-}
+    }
 
-C LookupWord(const char* s) {
+    C LookupWord(const char *s) {
 	C ptr = Get(LatestPtr);
 	while (ptr) {
-D(stderr, " <<lookupWord(%s) trying ptr=%d ", s, ptr);
-		char* name = &Mem[ptr + S];  // name follows link.
-		if (streq(s, name)) {
-			// code addr follows name and '\0'.
-			return ptr + S + strlen(name) + 1;
-		}
-		ptr = Get(ptr);
+	    D(stderr, " <<lookupWord(%s) trying ptr=%d ", s, ptr);
+	    char *name = &Mem[ptr + S];	// name follows link.
+	    if (streq(s, name)) {
+		// code addr follows name and '\0'.
+		return ptr + S + strlen(name) + 1;
+	    }
+	    ptr = Get(ptr);
 	}
 	return 0;
-}
+    }
 
-bool IsNumber(const char* s, C* out) {
+    bool IsNumber(const char *s, C * out) {
 	C z = 0;
 	C sign = 1;
 	if (*s == '-') {
-		sign = -1;
-		s++;
+	    sign = -1;
+	    s++;
 	}
 	while (*s) {
-		if ('0' <= *s && *s <= '9') {
-			z = 10*z + (*s - '0');
-		} else {
-			*out = 0;
-			return false;
-		}
-		++s;
+	    if ('0' <= *s && *s <= '9') {
+		z = 10 * z + (*s - '0');
+	    } else {
+		*out = 0;
+		return false;
+	    }
+	    ++s;
 	}
 	*out = z;
 	return true;
-}
+    }
 
-void Execute(const char* s) {
+    void Execute(const char *s) {
 	C x = LookupWord(s);
 	if (!x) {
-		if (IsNumber(s, &x)) {
-			Ds -= S;
-			Put(Ds, x);
-			D(stderr, " Literal(%lld) ", (long long) x);
-			return;
-		}
-		Fatal("No such word", 0);
+	    if (IsNumber(s, &x)) {
+		Ds -= S;
+		Put(Ds, x);
+		D(stderr, " Literal(%lld) ", (long long) x);
 		return;
+	    }
+	    Fatal("No such word", 0);
+	    return;
 	}
 	C op = Get(x);
 	D(stderr, " Execute(%s)@%lld(op%d) ", s, (long long) x, op);
 	if (0 < op && op < MemLen) {
-		if (Ops[op]) {
-			Ops[op]();
-		} else {
-			Fatal("undefined op", op);
-		}
+	    if (Ops[op]) {
+		Ops[op] ();
+	    } else {
+		Fatal("undefined op", op);
+	    }
 	} else {
-		Fatal("bad op", op);
+	    Fatal("bad op", op);
 	}
-}
+    }
 
-void Init() {
+    void Init() {
 	Mem = new char[MemLen];
 	memset(Mem, 0, MemLen);
 
@@ -151,50 +152,53 @@ void Init() {
 
 	MakeOp(PLUS, OpPLUS, "+");
 	MakeOp(DOT, OpDOT, ".");
-}
+    }
 
-int LineIndex;
-char* NextWordInLine() {
-	for (; LineIndex < LINELEN-1; LineIndex++)  {
-		if (Mem[LineIndex] == '\0') return nullptr;
-		if (Mem[LineIndex] <= 32) continue;
+    int LineIndex;
 
-		int WordStart = LineIndex;	
-		for(;LineIndex < LINELEN-1; LineIndex++) {
-			if (Mem[LineIndex] <= 32) {
-				Mem[LineIndex] = '\0';
-				LineIndex++;
-				return Mem+WordStart;
-			}
+    char *NextWordInLine() {
+	for (; LineIndex < LINELEN - 1; LineIndex++) {
+	    if (Mem[LineIndex] == '\0')
+		return nullptr;
+	    if (Mem[LineIndex] <= 32)
+		continue;
 
+	    int WordStart = LineIndex;
+	    for (; LineIndex < LINELEN - 1; LineIndex++) {
+		if (Mem[LineIndex] <= 32) {
+		    Mem[LineIndex] = '\0';
+		    LineIndex++;
+		    return Mem + WordStart;
 		}
-	}
-}
 
-void Run() {
+	    }
+	}
+    }
+
+    void Run() {
 	while (true) {
-		memset(Mem, 0, LINELEN);
-		fprintf(stderr, " ok ");
-		auto cp = fgets(Mem, LINELEN, stdin);
-		if (cp == nullptr) {
-			fprintf(stderr, " *EOF* \n");
-			break;
-		}
-		LineIndex = 0;
-		const char* s;
-		while (s = NextWordInLine()) {
-			D(stderr, "Word(%s) ", s);
-			Execute(s);
-			DumpMem();
-		}
+	    memset(Mem, 0, LINELEN);
+	    fprintf(stderr, " ok ");
+	    auto cp = fgets(Mem, LINELEN, stdin);
+	    if (cp == nullptr) {
+		fprintf(stderr, " *EOF* \n");
+		break;
+	    }
+	    LineIndex = 0;
+	    const char *s;
+	    while (s = NextWordInLine()) {
+		D(stderr, "Word(%s) ", s);
+		Execute(s);
+		DumpMem();
+	    }
 	}
-}
+    }
 
-void Flush() {
+    void Flush() {
 	delete Mem;
-}
+    }
 
-int Main(int argc, const char* argv[]) {
+    int Main(int argc, const char *argv[]) {
 	// printf("short %d\n", sizeof(short));
 	// printf("int %d\n", sizeof(int));
 	// printf("long %d\n", sizeof(long));
@@ -207,25 +211,26 @@ int Main(int argc, const char* argv[]) {
 	Argv0 = argv[0];
 	++argv, --argc;
 	while (argc > 0 && argv[0][0] == '-') {
-		switch (argv[0][1]) {
-		case 'm':
-			MemLen = atoi(&argv[0][2]);
-			break;
-		case 'x':
-			Debug = atoi(&argv[0][2]);
-			break;
-		default:
-			Fatal("Bad flag", argv[0][1]);
-		}
-		++argv, --argc;
+	    switch (argv[0][1]) {
+	    case 'm':
+		MemLen = atoi(&argv[0][2]);
+		break;
+	    case 'x':
+		Debug = atoi(&argv[0][2]);
+		break;
+	    default:
+		Fatal("Bad flag", argv[0][1]);
+	    }
+	    ++argv, --argc;
 	}
 
 	Init();
 	Run();
 	Flush();
-}
-} // namespace forth_yak
+    }
+}				// namespace forth_yak
 
-int main(int argc, const char* argv[]) {
-	return forth_yak::Main(argc, argv);
+int main(int argc, const char *argv[])
+{
+    return forth_yak::Main(argc, argv);
 }
--- forth-yak.h
+++ forth-yak.h
@@ -7,73 +7,70 @@
 #define D    if(Debug)fprintf
 
 namespace forth_yak {
-extern int Debug;
+    extern int Debug;
 
 #ifndef CELLSIZE
 #define CELLSIZE 4
 #endif
 
 #if CELLSIZE == 2
-typedef int16_t   C;  // Cell
-typedef uint16_t  U;  // Unsigned cell
+    typedef int16_t C;		// Cell
+    typedef uint16_t U;		// Unsigned cell
 #elif CELLSIZE == 4
-typedef int32_t   C;  // Cell
-typedef uint32_t  U;  // Unsigned cell
-typedef float     F;  // Floating point
+    typedef int32_t C;		// Cell
+    typedef uint32_t U;		// Unsigned cell
+    typedef float F;		// Floating point
 #elif CELLSIZE == 8
-typedef int64_t   C;  // Cell
-typedef uint64_t  U;  // Unsigned cell
-typedef double    F;  // Floating point
+    typedef int64_t C;		// Cell
+    typedef uint64_t U;		// Unsigned cell
+    typedef double F;		// Floating point
 #else
-error -- unknown cellsize
+     error-- unknown cellsize
 #endif
+    typedef unsigned char B;	// Byte
 
-typedef unsigned char B;  // Byte
+    constexpr size_t S = sizeof(C);
+    constexpr size_t LINELEN = 500;
 
-constexpr size_t S = sizeof(C);
-constexpr size_t LINELEN = 500;
+    extern char *Mem;
+    extern int MemLen;
 
-extern char* Mem;
-extern int MemLen;
-
-typedef union {
+    typedef union {
 	C c;
 	char b[S];
-} Both;
+    } Both;
 
-inline C Get(C i) {
+    inline C Get(C i) {
 	Both x;
-	memcpy(x.b, Mem+i, S);
-}
-
-inline void Put(C i, C x) {
+	 memcpy(x.b, Mem + i, S);
+    } inline void Put(C i, C x) {
 	Both z;
 	z.c = x;
-	memcpy(Mem+i, z.b, S);
-}
+	memcpy(Mem + i, z.b, S);
+    }
 
-extern C HerePtr;  // points to Here variable
-extern C LatestPtr;  // points to Latest variable
-extern C Ds;  // data stack ptr
-extern C Rs;  // return stack ptr
-extern C Ip;  // instruction ptr
-extern C W;  // W register
-extern const char* Argv0;
+    extern C HerePtr;		// points to Here variable
+    extern C LatestPtr;		// points to Latest variable
+    extern C Ds;		// data stack ptr
+    extern C Rs;		// return stack ptr
+    extern C Ip;		// instruction ptr
+    extern C W;			// W register
+    extern const char *Argv0;
 
-inline bool streq(const char* p, const char* q) {
+    inline bool streq(const char *p, const char *q) {
 	D(stderr, "  (streq <%s> <%s>) ", p, q);
 	return 0 == strcmp(p, q);
-}
+    }
 
-typedef enum {
+    typedef enum {
 	NONE_OpCode,
 	THREE,
 	EIGHT,
 	PLUS,
 	DOT,
-} Opcode;
+    } Opcode;
 
-typedef void OpFn();
-extern OpFn* Ops[LINELEN];
+    typedef void OpFn();
+    extern OpFn *Ops[LINELEN];
 
-} // namespace forth_yak
+}				// namespace forth_yak
